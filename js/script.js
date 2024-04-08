7127ff081bb2984b68591a172d9756446da3b165
Vladimiroskiv <serbea.vlad@gmail.com>
2024-04-08 22:16:18 +0300

    Updates and maintenance page preparation
    - Updated content and styling for blog page and individual posts to improve readability and aesthetics.
    - Added css/maintenance.css file to support maintenance page styling.
    - Improved overall styling in css/style.css for better design consistency across the site.
    - Modified JS scripts for performance optimizations and improved functionality.
    - Prepared maintenance.html to give visitors an elegant message during site updates.
    - Updated the services page to reflect the new offers.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuBtn = document.querySelector(".menu-btn");
     const navigation = document.querySelector(".navigation");
 
@@ -35,49 +35,49 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    document.getElementById('cookieConsentButton').addEventListener('click', function() {
+    document.getElementById('cookieConsentButton').addEventListener('click', function () {
         document.getElementById('cookieConsentContainer').style.display = 'none';
         localStorage.setItem('cookieConsent', 'true');
     });
 
-    if(localStorage.getItem('cookieConsent') === 'true'){
+    if (localStorage.getItem('cookieConsent') === 'true') {
         document.getElementById('cookieConsentContainer').style.display = 'none';
     } else {
         document.getElementById('cookieConsentContainer').style.display = 'block';
     }
 
- 
-    let startX, startY, dist, threshold = 150, 
-        allowedTime = 200, 
+
+    let startX, startY, dist, threshold = 150,
+        allowedTime = 200,
         elapsedTime, startTime;
 
     slides.forEach(slide => {
-        slide.addEventListener('touchstart', function(e){
+        slide.addEventListener('touchstart', function (e) {
             const touchObj = e.changedTouches[0];
             dist = 0;
             startX = touchObj.pageX;
             startY = touchObj.pageY;
-            startTime = new Date().getTime(); 
+            startTime = new Date().getTime();
             e.preventDefault();
         }, false);
 
-        slide.addEventListener('touchmove', function(e){
+        slide.addEventListener('touchmove', function (e) {
             e.preventDefault();
         }, false);
 
-        slide.addEventListener('touchend', function(e){
+        slide.addEventListener('touchend', function (e) {
             const touchObj = e.changedTouches[0];
-            dist = touchObj.pageX - startX; 
-            elapsedTime = new Date().getTime() - startTime; 
-            if (elapsedTime <= allowedTime){ 
+            dist = touchObj.pageX - startX;
+            elapsedTime = new Date().getTime() - startTime;
+            if (elapsedTime <= allowedTime) {
                 let swipeIndex = slides.length;
-                if (Math.abs(dist) >= threshold){
+                if (Math.abs(dist) >= threshold) {
                     swipeIndex = dist < 0 ? 1 : -1;
                 }
                 let currentIndex = Array.from(slides).indexOf(document.querySelector('.video-slide.active'));
                 let newIndex = currentIndex + swipeIndex;
                 if (newIndex < 0) newIndex = slides.length - 1;
-                else newIndex = newIndex % slides.length; 
+                else newIndex = newIndex % slides.length;
                 sliderNav(newIndex);
             }
             e.preventDefault();
