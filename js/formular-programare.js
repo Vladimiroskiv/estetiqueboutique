7d4ddece82b1c5929b49a9d11bc2329151981a45
Vladimiroskiv <serbea.vlad@gmail.com>
2024-04-16 13:13:58 +0300

    fix swipe
    modified:   blog/postare1.html
    modified:   index.html
    modified:   js/formular-programare.js
    modified:   js/script.js
    modified:   programari/programare.css
    modified:   programari/programare.html

--- js/formular-programare.js
+++ js/formular-programare.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const serviciiSelect = document.getElementById('servicii');
     const pretServiciuInput = document.getElementById('pret_serviciu');
+    const formularProgramare = document.getElementById('formular-programare');
 
+    
     serviciiSelect.addEventListener('change', function () {
         const pret = this.options[this.selectedIndex].dataset.price || '0';
         pretServiciuInput.value = pret + ' RON';
@@ -11,4 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
         const initialPrice = serviciiSelect.options[serviciiSelect.selectedIndex].dataset.price || '0';
         pretServiciuInput.value = initialPrice + ' RON';
     }
+
+    
+    formularProgramare.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        
+
+        alert('Programarea s-a realizat cu succes!');
+        window.location.href = '../index.html';
+    });
 });
